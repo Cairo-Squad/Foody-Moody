e504129e40ba08ec07b3accad2314349beb02234
ahmed samy <ahmbdelkhalek@gmail.com>
2025-04-16 13:25:58 +0200

    refactor: change types of some data

--- src/model/Meal.kt
+++ src/model/Meal.kt
@@ -1,17 +1,19 @@
 package model
 
+import java.time.LocalDate
+
 
 data class Meal(
     val mealName:String?,
-    val mealId: String?,
+    val mealId: Int?,
     val mealDescription: String? = null,
-    val contributorId: String? = null,
-    val minutes: String? = null,
-    val submitted: String? = null,
+    val contributorId: Int? = null,
+    val minutes: Int? = null,
+    val submitted: LocalDate? = null,
     val tags: List<String>? = null,
     val nutrition: Nutrition? = null,
     val numberOfSteps: String? = null,
     val steps: List<String>? = null,
     val ingredients: List<String>? = null,
-    val numberOfIngredients: String? = null,
+    val numberOfIngredients: Int? = null,
 )
