e8eece13839893c626dd06166da9f34690b10d4d
ahmed samy <ahmbdelkhalek@gmail.com>
2025-04-16 19:39:14 +0200

    refactor: move the implementation to the repo

--- src/logic/ExploreOtherCountriesFoodCultureUseCase.kt
+++ src/logic/ExploreOtherCountriesFoodCultureUseCase.kt
@@ -9,19 +9,7 @@ class ExploreOtherCountriesFoodCultureUseCase(
     fun getTwentyRandomMealByCountry(
          countryName: String
     ): List<Meal> {
-        val list = mutableListOf<Meal>()
-        val searchTerm = countryName.trim().lowercase()
-
-        repo.getAllMeals().forEach { meal ->
-            if (meal.tags?.any { tag ->
-                    tag.trim().lowercase() == searchTerm
-                } == true) {
-                list.add(meal)
-            }
-        }
-
-        return list.shuffled().take(20)
-
+        return repo.getTwentyRandomMealByCountry(countryName)
     }
 
 }
