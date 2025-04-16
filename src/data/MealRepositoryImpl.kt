6ae9daf10521c4bf7d657fc83bd24b8047e5ebb2
ahmed samy <ahmbdelkhalek@gmail.com>
2025-04-16 19:39:56 +0200

    refactor: add function getTwentyRandomMealByCountry implementation

--- src/data/MealRepositoryImpl.kt
+++ src/data/MealRepositoryImpl.kt
@@ -26,4 +26,20 @@ class MealRepositoryImpl(
         return listOfMeals
     }
 
+
+    override fun getTwentyRandomMealByCountry(countryName: String): List<Meal> {
+        val list = mutableListOf<Meal>()
+        val searchTerm = countryName.trim().lowercase()
+
+        getAllMeals().forEach { meal ->
+            if (meal.tags?.any { tag ->
+                    tag.trim().lowercase() == searchTerm
+                } == true) {
+                list.add(meal)
+            }
+        }
+
+        return list.shuffled().take(20)
+    }
+
 }
