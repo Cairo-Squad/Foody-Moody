4d8d8f81e8ab6d0c3f350750d95463eaeabdfdf4
Abdo Sharaf <bodyasharaf8@gmail.com>
2025-04-16 11:13:25 +0200

    feat: implement base structure

---
+++ src/presentation/cliController/CLIDispatcher.kt
@@ -0,0 +1,24 @@
+package presentation.cliController
+
+class CLIDispatcher {
+
+    // TODO: Map your feature's command code to its function here
+    private val commands = mapOf<Int, () -> Unit>(
+
+    )
+
+    fun dispatch(userInput: Int) {
+        val command = commands[userInput]
+        if (command != null) {
+            command()
+        } else {
+            println(CLIConstants.COMMON_ERROR_MESSAGE)
+        }
+    }
+
+    fun validateOption(option: Int): Boolean {
+        return option == CLIConstants.EXIT_COMMAND_CODE || option in commands.keys
+    }
+
+    // TODO: Implement your feature here as a private function and map it in the above map
+}
