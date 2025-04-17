96cf0d4c5432afd9ee045c243035a0e283d39cc5
Ahmed Mahgoub <aed7862@gmail.com>
2025-04-17 14:59:06 +0200

    feat: add feature suggest ten random easy food meals

--- src/logic/GetRandomEasyFoodMealsUseCase.kt
+++ src/logic/GetRandomEasyFoodMealsUseCase.kt
@@ -7,15 +7,21 @@ class GetRandomEasyFoodMealsUseCase(
 ) {
     fun getRandomEasyFoodMeals(): List<Meal> {
         return mealRepository.getAllMeals()
-            .filter(::isEasyFoodMeal)
+            .filter { isHighQualityMeal(it) && isEasyFoodMeal(it) }
             .shuffled()
             .take(10)
     }
 
     private fun isEasyFoodMeal(meal: Meal): Boolean {
-        return meal.mealName != null
-                && meal.minutes!! <= LogicConstants.EASY_FOOD_MINIMUM_MINUTES
+        return meal.minutes!! <= LogicConstants.EASY_FOOD_MINIMUM_MINUTES
                 && meal.numberOfIngredients!! <= LogicConstants.EASY_FOOD_MINIMUM_INGREDIENTS
-                && meal.numberOfSteps!!.toInt() <= LogicConstants.EASY_FOOD_MINIMUM_STEPS
+                && meal.numberOfSteps!! <= LogicConstants.EASY_FOOD_MINIMUM_STEPS
+    }
+
+    private fun isHighQualityMeal(meal: Meal): Boolean {
+        return meal.mealName != null
+                && meal.minutes != null
+                && meal.numberOfIngredients != null
+                && meal.numberOfSteps != null
     }
 }
