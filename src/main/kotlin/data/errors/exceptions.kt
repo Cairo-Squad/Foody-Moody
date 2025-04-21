fd2fc9a94fee7bb2ee81e97541430beda2a44ed9
Ahmed Mahgoub <aed7862@gmail.com>
2025-04-21 17:40:38 +0200

    refactor: handle exception for not easy food meals found

--- src/main/kotlin/data/errors/exceptions.kt
+++ src/main/kotlin/data/errors/exceptions.kt
@@ -3,3 +3,5 @@ package data.errors
 class ValidationException(message: String) : Exception(message)
 
 class NoResultException(message: String) : Exception(message)
+
+class EasyFoodMealsNotFoundException(message: String): Exception(message)
