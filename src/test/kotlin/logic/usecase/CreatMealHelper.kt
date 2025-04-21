08331bb2a66e07123518191645ff919cd26b5e10
norhanadel367 <norhanadel367@gmail.com>
2025-04-21 17:49:40 +0200

    refactor:edit formating file CreateMealHelper.kt

--- src/test/kotlin/logic/usecase/CreatMealHelper.kt
+++ src/test/kotlin/logic/usecase/CreatMealHelper.kt
@@ -4,20 +4,20 @@ import logic.model.Meal
 import logic.model.Nutrition
 
 fun createMealOfSeafood(
-    mealName : String?=null ,
-    mealId:Int?=null,
-    tags: List<String>? = null ,
-    protein: Float? = null ,
-)= Meal(
-    mealId=mealId ,
-    mealName = mealName,
+    mealName : String? = null ,
+    mealId : Int? = null ,
+    tags : List<String>? = null ,
+    protein : Float? = null ,
+) = Meal(
+    mealId = mealId ,
+    mealName = mealName ,
     mealDescription = null ,
-    contributorId=null ,
+    contributorId = null ,
     minutes = null ,
-    submitted=null ,
-    tags=tags ,
-    nutrition = Nutrition(null, null, null, null, protein, null, null),
-    numberOfSteps=null ,
+    submitted = null ,
+    tags = tags ,
+    nutrition = Nutrition(null , null , null , null , protein , null , null) ,
+    numberOfSteps = null ,
     steps = null ,
     ingredients = null ,
     numberOfIngredients = null
