cd1fc8082cad963bbc9be08875273a5d630a90d4
Abdo Sharaf <bodyasharaf8@gmail.com>
2025-04-21 03:43:38 +0200

    feat: use koin to manage dependency injection

--- src/main/kotlin/data/csvUtil/CSVConstants.kt
+++ src/main/kotlin/data/csvUtil/CSVConstants.kt
@@ -1,4 +1,4 @@
-package presentation
+package data.csvUtil
 
 object CSVConstants {
 
--- src/main/kotlin/presentation/Main.kt
+++ src/main/kotlin/presentation/Main.kt
@@ -1,48 +1,19 @@
 package presentation
 
-import data.DataSourceImpl
-import logic.usecase.GetIraqMealsUseCase
-import data.csvUtil.MealCsvParser
-import data.csvUtil.MealCsvReader
-import data.MealRepositoryImpl
+import di.appModule
+import di.useCaseModule
 import presentation.cliController.CLIController
-import presentation.cliController.CLIDispatcher
-import logic.usecase.GetRandomMealUseCase
-import logic.usecase.IngredientsGameUseCase
-import logic.usecase.SearchMealByNameUseCase
-import logic.usecase.*
+import org.koin.core.context.startKoin
+import org.koin.java.KoinJavaComponent.getKoin
 import presentation.cliController.CLIConstants
-import java.io.File
 
 fun main() {
-    val mealCsvReader = MealCsvReader(File(CSVConstants.CSV_FILE_PATH))
-    val mealCsvParser = MealCsvParser()
+    startKoin {
+        modules(appModule, useCaseModule)
+    }
 
     println(CLIConstants.LOADING_MEALS_DATA_MESSAGE)
-    val dataSource = DataSourceImpl(
-        mealCsvParser = mealCsvParser,
-        mealCsvReader = mealCsvReader
-    )
-    val mealRepository = MealRepositoryImpl(dataSource)
 
-    val cliDispatcher = CLIDispatcher(
-        searchMealByName = SearchMealByNameUseCase(mealRepository),
-        getMealsForLargeGroupUseCase = GetMealsForLargeGroupUseCase(mealRepository),
-        randomMealUseCase = GetRandomMealUseCase(mealRepository),
-        getIraqMealsUseCase = GetIraqMealsUseCase(mealRepository),
-        randomPotatoMealsUseCase = RandomPotatoMealsUseCase(mealRepository),
-        getMealsMoreThan700CaloriesUseCase = GetMealsMoreThan700CaloriesUseCase(mealRepository),
-        exploreOtherCountriesFoodCultureUseCase = ExploreOtherCountriesFoodCultureUseCase(mealRepository),
-        getRandomEasyFoodMealsUseCase = GetRandomEasyFoodMealsUseCase(mealRepository),
-        ingredientsGameUseCase = IngredientsGameUseCase(mealRepository),
-        suggestMealsToGymUseCase = SuggestMealsToGymUseCase(mealRepository),
-        getMealsByDateUseCase = GetMealsByDateUseCase(mealRepository),
-        getSeafoodMealsSortedByProteinUseCase = GetSeafoodMealsSortedByProteinUseCase(mealRepository),
-        getHealthyFastFoodUseCase = GetHealthyFastFoodUseCase(mealRepository),
-        sweetsNoEggsUseCase = SweetsNoEggsUseCase(mealRepository),
-        ketoMealUseCase = KetoMealUseCase(mealRepository),
-    )
-
-    val cliController = CLIController(cliDispatcher)
+    val cliController: CLIController = getKoin().get()
     cliController.start()
 }
