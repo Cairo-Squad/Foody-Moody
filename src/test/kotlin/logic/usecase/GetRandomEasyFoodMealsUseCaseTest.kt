9b2449054ee0a69e9336b738c64b0376b1ec0045
Ahmed Mahgoub <aed7862@gmail.com>
2025-04-23 17:08:10 +0200

    test: separate meals list mocks implementation from test

--- src/test/kotlin/logic/usecase/GetRandomEasyFoodMealsUseCaseTest.kt
+++ src/test/kotlin/logic/usecase/GetRandomEasyFoodMealsUseCaseTest.kt
@@ -34,43 +34,44 @@ class GetRandomEasyFoodMealsUseCaseTest {
     @Test
     fun `should ignore low quality meals when name is missing`() {
         // Given
-        every { mealRepository.getAllMeals() } returns mealsWithMissingName()
+        every { mealRepository.getAllMeals() } returns nonValidEasyFoodMeals() + lessThan10EasyFoodMeals()
+
 
         // When
         val result = getRandomEasyFoodMealsUseCase.getRandomEasyFoodMeals()
 
         // Then
-        assertThat(result).hasSize(1)
+        assertThat(result).hasSize(lessThan10EasyFoodMeals().size)
     }
 
     @Test
     fun `should ignore not easy food meals when missing one requirement`() {
         // Given
-        every { mealRepository.getAllMeals() } returns mealsWithMissingRequirements()
+        every { mealRepository.getAllMeals() } returns nonValidEasyFoodMeals() + lessThan10EasyFoodMeals()
 
         // When
         val result = getRandomEasyFoodMealsUseCase.getRandomEasyFoodMeals()
 
         // Then
-        assertThat(result).hasSize(1)
+        assertThat(result).hasSize(lessThan10EasyFoodMeals().size)
     }
 
     @Test
     fun `should retrieve only easy food meals when filtering meals`() {
         // Given
-        every { mealRepository.getAllMeals() } returns mixedMeals()
+        every { mealRepository.getAllMeals() } returns nonValidEasyFoodMeals() + lessThan10EasyFoodMeals()
 
         // When
         val result = getRandomEasyFoodMealsUseCase.getRandomEasyFoodMeals()
 
         // Then
-        assertThat(result).hasSize(1)
+        assertThat(result).hasSize(2)
     }
 
     @Test
     fun `getRandomEasyFoodMeals should return exactly 10 meals when more than 10 matching meals exist`() {
         // Given
-        every { mealRepository.getAllMeals() } returns moreThan10EasyMeals()
+        every { mealRepository.getAllMeals() } returns easyFoodMeals()
 
         // When
         val result = getRandomEasyFoodMealsUseCase.getRandomEasyFoodMeals()
@@ -88,13 +89,13 @@ class GetRandomEasyFoodMealsUseCaseTest {
         val result = getRandomEasyFoodMealsUseCase.getRandomEasyFoodMeals()
 
         // Then
-        assertThat(result).hasSize(3)
+        assertThat(result).hasSize(lessThan10EasyFoodMeals().size)
     }
 
     @Test
     fun `getRandomEasyFoodMeals should throw EasyFoodMealsNotFoundException when no easy food meals exist`() {
         // Given
-        every { mealRepository.getAllMeals() } returns noEasyFoodMeals()
+        every { mealRepository.getAllMeals() } returns nonValidEasyFoodMeals()
 
         // When & Then
         assertThrows<NoSuchElementException> {
@@ -105,7 +106,7 @@ class GetRandomEasyFoodMealsUseCaseTest {
     @Test
     fun `getRandomEasyFoodMeals should return shuffled meals every time when called`() {
         // Given
-        every { mealRepository.getAllMeals() } returns moreThan10EasyMeals()
+        every { mealRepository.getAllMeals() } returns easyFoodMeals()
 
         // When
         val firstResult = getRandomEasyFoodMealsUseCase.getRandomEasyFoodMeals()
@@ -114,7 +115,7 @@ class GetRandomEasyFoodMealsUseCaseTest {
         // Then
         assertThat(firstResult).isNotEqualTo(secondResult)
     }
-
+/*
     private fun mealsWithMissingName() = listOf(
         Meal(mealName = null, minutes = 20, numberOfIngredients = 5, numberOfSteps = 5),
         Meal(mealName = "pizza", minutes = 20, numberOfIngredients = 5, numberOfSteps = 5),
@@ -146,19 +147,35 @@ class GetRandomEasyFoodMealsUseCaseTest {
         Meal(mealName = "Pasta", minutes = 15, numberOfIngredients = 3, numberOfSteps = 3),
         Meal(mealName = "Salad", minutes = 10, numberOfIngredients = 2, numberOfSteps = 2)
     )
+    */
 
-    private fun moreThan10EasyMeals(): List<Meal> = listOf(
-        Meal(mealName = "Pasta", minutes = 15, numberOfIngredients = 3, numberOfSteps = 3),
+    private fun easyFoodMeals(): List<Meal> = listOf(
         Meal(mealName = "Salad", minutes = 10, numberOfIngredients = 2, numberOfSteps = 2),
-        Meal(mealName = "Sandwich", minutes = 5, numberOfIngredients = 2, numberOfSteps = 1),
-        Meal(mealName = "Scrambled Eggs", minutes = 7, numberOfIngredients = 3, numberOfSteps = 2),
-        Meal(mealName = "Instant Noodles", minutes = 3, numberOfIngredients = 2, numberOfSteps = 2),
-        Meal(mealName = "Toast", minutes = 5, numberOfIngredients = 2, numberOfSteps = 1),
-        Meal(mealName = "Fruit Salad", minutes = 5, numberOfIngredients = 3, numberOfSteps = 2),
-        Meal(mealName = "Grilled Cheese", minutes = 10, numberOfIngredients = 3, numberOfSteps = 2),
         Meal(mealName = "Smoothie", minutes = 5, numberOfIngredients = 4, numberOfSteps = 1),
         Meal(mealName = "Egg Salad", minutes = 10, numberOfIngredients = 3, numberOfSteps = 2),
         Meal(mealName = "Yogurt Parfait", minutes = 5, numberOfIngredients = 3, numberOfSteps = 2),
-        Meal(mealName = "Rice and Beans", minutes = 15, numberOfIngredients = 3, numberOfSteps = 3)
+        Meal(mealName = "Pasta", minutes = 15, numberOfIngredients = 3, numberOfSteps = 3),
+        Meal(mealName = "Rice and Beans", minutes = 15, numberOfIngredients = 3, numberOfSteps = 3),
+        Meal(mealName = "Salad", minutes = 10, numberOfIngredients = 10, numberOfSteps = 2),
+        Meal(mealName = "Salad", minutes = 10, numberOfIngredients = 2, numberOfSteps = 2),
+        Meal(mealName = "Smoothie", minutes = 5, numberOfIngredients = 4, numberOfSteps = 1),
+        Meal(mealName = "Egg Salad", minutes = 10, numberOfIngredients = 3, numberOfSteps = 2),
+        Meal(mealName = "Yogurt Parfait", minutes = 5, numberOfIngredients = 3, numberOfSteps = 2),
+        Meal(mealName = "Pasta", minutes = 15, numberOfIngredients = 3, numberOfSteps = 3),
+    )
+
+    private fun lessThan10EasyFoodMeals() = listOf(
+        Meal(mealName = "Smoothie", minutes = 5, numberOfIngredients = 4, numberOfSteps = 1),
+        Meal(mealName = "Egg Salad", minutes = 10, numberOfIngredients = 3, numberOfSteps = 2),
+    )
+
+    private fun nonValidEasyFoodMeals() = listOf(
+        Meal(mealName = null, minutes = 20, numberOfIngredients = 5, numberOfSteps = 5),
+        Meal(mealName = "Pizza", minutes = null, numberOfIngredients = 5, numberOfSteps = 5),
+        Meal(mealName = "Pizza", minutes = 20, numberOfIngredients = null, numberOfSteps = 5),
+        Meal(mealName = "Pizza", minutes = 20, numberOfIngredients = 5, numberOfSteps = null),
+        Meal(mealName = "Pizza", minutes = 35, numberOfIngredients = 4, numberOfSteps = 4),
+        Meal(mealName = "Pizza", minutes = 20, numberOfIngredients = 8, numberOfSteps = 4),
+        Meal(mealName = "Pizza", minutes = 20, numberOfIngredients = 4, numberOfSteps = 8)
     )
 }
