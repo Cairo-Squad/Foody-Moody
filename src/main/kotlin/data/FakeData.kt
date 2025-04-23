f993e02a6f1ce025ee285c20a40c789e783b4c43
norhanadel367 <norhanadel367@gmail.com>
2025-04-23 22:42:33 +0200

    refactor: edite in FakeData

--- src/main/kotlin/data/FakeData.kt
+++ src/main/kotlin/data/FakeData.kt
@@ -11,16 +11,13 @@ object FakeData {
                 Meal(mealId = 3, tags = listOf("lunch"), nutrition = Nutrition(carbohydrates = 8.0f, protein = 15.0f, totalFat = 13.0f)),
                 Meal(mealId = 4, tags = listOf("dinner"), nutrition = Nutrition(carbohydrates = 11.0f, protein = 14.0f, totalFat = 11.0f)),
                 Meal(mealId = 5, tags = listOf("dinner"), nutrition = Nutrition(carbohydrates = 6.0f, protein = 25.0f, totalFat = 20.0f)),
-                Meal(mealId = 6, tags = listOf("lunch"), nutrition = null),
-                Meal(mealId = 100, tags = listOf("keto"), nutrition = Nutrition(carbohydrates = 5.0f, protein = 14.0f, totalFat = 15.0f)),
-                Meal(mealId = 101, tags = listOf("keto"), nutrition = Nutrition(carbohydrates = 5.0f, protein = 20.0f, totalFat = 11.0f)),
-                Meal(mealId = 102, tags = listOf("keto"), nutrition = Nutrition(carbohydrates = 5.0f, protein = 15.0f, totalFat = 12.0f))
+                Meal(mealId = 6, tags = listOf("lunch"), nutrition = null)
     )
 
     val testMealsOfMinProteinAndTotalFat= listOf(
-        Meal(mealId = 100, tags = listOf("keto"), nutrition = Nutrition(carbohydrates = 5.0f, protein = 14.0f, totalFat = 15.0f)) ,
-        Meal(mealId = 101, tags = listOf("keto"), nutrition = Nutrition(carbohydrates = 5.0f, protein = 20.0f, totalFat = 11.0f),),
-        Meal(mealId = 102, tags = listOf("keto"), nutrition = Nutrition(carbohydrates = 9.5f, protein = 15.0f, totalFat = 12.0f))
+        Meal(mealId = 1, tags = listOf("dinner"), nutrition = Nutrition(carbohydrates = 5.0f, protein = 14.0f, totalFat = 15.0f)) ,
+        Meal(mealId = 2, tags = listOf("lunch"), nutrition = Nutrition(carbohydrates = 5.0f, protein = 20.0f, totalFat = 11.0f),),
+        Meal(mealId = 3, tags = listOf("lunch"), nutrition = Nutrition(carbohydrates = 9.5f, protein = 15.0f, totalFat = 12.0f))
     )
     val nonKetoMeals = listOf(
         Meal(mealId=1, tags =  listOf("lunch"), nutrition = Nutrition(carbohydrates=20.0f,protein=  10.0f,totalFat= 5.0f)),
